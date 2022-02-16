8969d10dcd39315f924a18bd2b5646db7cd5062d
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-02-15 22:50:36 -0500

    Added profile picture

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Projects from "./Components/Projects.js"; 
 import Introduction from "./Components/Introduction.js"; 
 import './index.css'
+import Skills from "./Components/Skills.js"; 
 
 function App() {
   return (
     <div className="App">
       <Introduction />
       <Projects />
+      <Skills />
     </div>
   );
 }
---
+++ src/Components/Skills.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Skills() {
+  return (
+    <>
+        <h3>Languages and Frameworks</h3>
+        <p>C#, React</p>
+        
+    </>
+  )
+}
+
+export default Skills
