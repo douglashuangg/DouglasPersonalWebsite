4dc505a833a8ab33b2cc264b9e2e2577cbd0823a
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-02-16 21:47:23 -0500

    fixed iamge

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react'; 
 
 function Introduction() {
   return (
     <div className="introduction">
-      <div className="logo" alt="logo" id="Douglas" />
+      <img src={`${process.env.PUBLIC_URL}/images/dp.jpg`} id="douglas" alt="Douglas Huang"/>
       <h1>Douglas Huang</h1>
  
       <a href="https://www.linkedin.com/in/douglas-huang-b8b521209/">Linkedin </a>
