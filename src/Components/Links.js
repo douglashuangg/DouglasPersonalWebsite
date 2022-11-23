4b0a8547d67707baea3240e74f5e547f6aa2acfa
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-11-23 01:06:35 -0500

    fixed link styling

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -1,16 +1,34 @@
-import React from 'react'; 
-import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import "../App.css";
 
 function Links() {
   return (
     <div>
-      <a href="https://github.com/douglashuangg" target="_blank" className="icons" style={{padding: "30px 30px 0 10px"}}><FontAwesomeIcon icon={faGithub} /> </a>
-      <a href="https://www.linkedin.com/in/douglas-huang-b8b521209/" target="_blank" className="icons"><FontAwesomeIcon icon={faLinkedin} /> </a>
-        
+      <a
+        href="https://github.com/douglashuangg"
+        target="_blank"
+        className="icons"
+        style={{ padding: "30px 30px 0 10px" }}
+      >
+        <div className="link-buttons">
+          <FontAwesomeIcon icon={faGithub} />{" "}
+        </div>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/douglas-huang-b8b521209/"
+        target="_blank"
+        className="icons"
+      >
+        <div className="link-buttons">
+          <FontAwesomeIcon icon={faLinkedin} />{" "}
+        </div>
+      </a>
+      {/* </div> */}
     </div>
-  )
+  );
 }
 
-export default Links
+export default Links;
